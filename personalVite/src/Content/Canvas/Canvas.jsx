e0a745c4c7e1b8d93d31fd370e6933e21edf7c77
Danny <bajanboss132@gmail.com>
2023-01-22 17:14:35 -0700

    Resized canvas and added an event listener to the canvas.

--- personalVite/src/Content/Canvas/Canvas.jsx
+++ personalVite/src/Content/Canvas/Canvas.jsx
@@ -1,3 +1,4 @@
+import "./Canvas.css"
 import { useRef, useEffect } from 'react';
 
 const Canvas = props => {
@@ -7,19 +8,32 @@ const Canvas = props => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
 
+        console.clear();
+        const dimensions = 
+            {
+                "width" : `${context.canvas.width}`,
+                "height" : `${context.canvas.height}`,
+            }
+        
+        console.table( dimensions )
+
         context.fillStyle = "#000"
         context.fillRect(0,0, context.canvas.width, context.canvas.height)
         context.fillStyle = "#fff"
         context.fillRect(0,0, 10, 10)
+
+        canvas.addEventListener("click", () => {
+            console.log("oh my god")
+        })
     }, [])
 
 
     return (
-        <>
+        <div className="canvas-container">
             <p>This is where the canvas would be </p>
             <noscript>Javascript must be enabled for this game to play.</noscript>
-            <canvas ref={canvasRef}></canvas>
-        </>
+            <canvas id="game" ref={canvasRef} height="300" width="400"></canvas>
+        </div>
     )
 }
 
