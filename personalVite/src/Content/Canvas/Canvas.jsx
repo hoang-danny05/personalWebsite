6cfc4c6995cc2e39c6eedfe26df0b7ff09caabc2
Danny <bajanboss132@gmail.com>
2023-01-24 20:54:44 -0700

    added an automated animation loop added a start button to start the loop refactored the sprite rendering process

--- personalVite/src/Content/Canvas/Canvas.jsx
+++ personalVite/src/Content/Canvas/Canvas.jsx
@@ -7,11 +7,15 @@ class Canvas extends React.Component {
     constructor(props) {
         super(props)
         this.canvasRef = React.createRef();
+        this.gameStarted = false;
     }
 
     componentDidMount() {
         const canvas = this.canvasRef.current
-        const context = canvas.getContext('2d')
+        const context = canvas.getContext('2d');
+        this.context = context;
+
+        this.gameStarted = false;
 
         // console.clear();
         const dimensions = 
@@ -29,20 +33,45 @@ class Canvas extends React.Component {
 
         const player = new Sprite({x: 30, y: 50})
         const enemy = new Sprite({x: 200, y:50})
+        this.sprites = [player, enemy]
 
         canvas.addEventListener("click", () => {
             //reset the background
-            context.fillStyle = "#000"
-            context.fillRect(0,0, context.canvas.width, context.canvas.height)
+            // context.fillStyle = "#000"
+            // context.fillRect(0,0, context.canvas.width, context.canvas.height)
             //update the players
-            context.fillStyle = "#00f"
-            player.draw(context)
-            player.update()
-            enemy.draw(context)
-            enemy.update()
+            // context.fillStyle = "#00f"
+            // player.draw(context)
+            // player.update()
+            // enemy.draw(context)
+            // enemy.update()
             console.log("Clicked")
             console.table(player);
         })
+
+        const playButton = document.getElementById("playbutton");
+
+        playButton.addEventListener("click", () => {
+            console.log(playButton.name)
+            this.gameStarted = true;
+            this.animate()
+        })
+        console.log(this)
+    }
+
+    animate = () => {
+        console.log(this.context)
+        //draw background
+        this.context.fillStyle = "#000"
+        this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        //draw all sprites
+        this.context.fillStyle = "#f00"
+        for (const sprite of this.sprites) {
+            sprite.draw(this.context);
+            sprite.update();
+        }
+        
+        window.requestAnimationFrame(this.animate)
     }
 
     render() {
@@ -58,7 +87,7 @@ class Canvas extends React.Component {
                 <div className="game-bar">
                     <div className="bar-item"><ion-icon name="expand-outline" /></div>
                     <div className="bar-item">Cool Game I Made</div>
-                    <div className="bar-item">-Danny</div>
+                    <div className="bar-item"><ion-icon name="play" id="playbutton"/></div>
                 </div>
                 <noscript>Javascript must be enabled for this game to play.</noscript>
                 <canvas id="game" ref={this.canvasRef} height="300" width="400"></canvas>
