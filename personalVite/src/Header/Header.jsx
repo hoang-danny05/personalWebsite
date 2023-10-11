2dbe099526533ae647f6364cc1cb37dfdd07b9f6
hoang-danny05 <bajanboss132@gmail.com>
2023-10-11 14:09:40 -0700

    added amazing typing effect on header click

--- personalVite/src/Header/Header.jsx
+++ personalVite/src/Header/Header.jsx
@@ -1,13 +1,33 @@
-import "../assets/react.svg"
+import "../assets/react.svg";
+import { useState } from 'react';
+
+let textIndex = 0;
+const textResult = "Danny Hoang, _ Software Programmer";
+const typingDelay = 50;
 
 function Header() {
+    const [innerHeader, setInnerHeader] = useState("Danny <br /> Hoang")
+
+    function doTypingEffect() {
+        if (textIndex > textResult.length) {
+            textIndex = 0;
+            return
+        }
+        //actually do the thing now
+        const newHeader = textResult.substring(0, textIndex);
+        setInnerHeader(newHeader.replace("_", "<br />"))
+        textIndex++;
+        setTimeout(doTypingEffect, typingDelay)
+    }
+    
     return(
         <div className='Header'>
-            <h1>Danny</h1> 
-            <h1> Hoang</h1>
+            <h1 className="name" onClick={doTypingEffect} dangerouslySetInnerHTML={{__html: innerHeader}}>
+            </h1>
             <img src="./src/assets/react.svg" alt="React Framework Logo"></img>
         </div>
     )
 }
+
 
 export default Header
